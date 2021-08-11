f0e86a35c3d58f4cc0349e7da8c550f6ebcc566f
Zoran Latinovic <Zoran.Latinovic@kraftwer.co.at>
2021-08-11 19:44:29 +0200

    end of section 5

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,76 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import { v4 as uuid } from  'uuid';
+
+import Navbar from './navbar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+
+function App() {
+
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
+
+  useEffect(() => {
+    async function fetchActivities() {
+      try {
+        const activitiesResponse = await axios.get<Activity[]>('http://localhost:5000/api/activities');
+        setActivities(activitiesResponse.data);
+      } catch (error) {
+        setActivities([]);
+      }
+    }
+
+    fetchActivities();
+  }, []);
+
+
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find(x => x.id === id));
+  }
+
+  function handleCancelSelectActivity() { setSelectedActivity(undefined); }
+
+  function handleFormOpen(id?: string) {
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose() { setEditMode(false)  }
+
+  function handleCreateOrEdit(activity: Activity){
+    activity.id ? 
+    setActivities([...activities.filter(x => x.id !== activity.id), activity]) :
+    setActivities([...activities, {...activity, id: uuid()}]);
+
+    setEditMode(false);
+    setSelectedActivity(activity);
+  }
+
+  function handleDeleteActivity(id:string){
+    setActivities([...activities.filter(x => x.id !== id)]);
+  }
+
+  return (
+    <Fragment>
+      <Navbar openForm={handleFormOpen} />
+      <Container style={{ marginTop: '7em' }}>
+        <ActivityDashboard
+          activities={activities}
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}
+          createOrEdit={handleCreateOrEdit}
+          deleteActivity={handleDeleteActivity}
+        />
+      </Container>
+    </Fragment>
+  );
+}
+
+export default App;
